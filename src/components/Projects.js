095d463b8e45ec3dc586bddc166129d6e46488ea
suthari sai manikanta vivek <115859708+Mani9505764142@users.noreply.github.com>
2025-05-16 20:58:53 +0530

    Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,28 +4,25 @@ const projects = [
     description:
       'Designed and developed a responsive portfolio website to showcase my skills, projects, and contact information. The website features a clean, modern design with smooth navigation and is built using modern web technologies.',
     technologies: ['React', 'HTML', 'CSS', 'JavaScript', 'AWS (S3, CloudFront)'],
-    link: 'https://your-portfolio-url.com', 
+    link: 'https://your-portfolio-url.com',
   },
   {
     title: 'Face Recognition',
     description:
       'Developed a software for a biometric application using Python and machine learning tools. The system enables biometric devices to recognize the face of an employee and capture their login and logout times.',
     technologies: ['Python', 'Machine Learning Tools'],
-    link: '',
   },
   {
     title: 'Human Action Recognition From Depth Maps And Postures',
     description:
       'Utilized Convolutional Neural Networks (CNNs) to recognize human actions from depth maps, achieving high accuracy and robust performance. The model has potential applications in healthcare, gaming, and human-computer interaction.',
     technologies: ['Convolutional Neural Networks (CNNs)', 'Python', 'Machine Learning Frameworks'],
-    link: '',
   },
   {
     title: 'AWS Multi-Tier Application',
     description:
       'Architected and implemented a scalable e-commerce platform on AWS within a VPC. Utilized EC2, S3, and RDS to ensure high availability and performance. Implemented private and public subnets, secured by security groups, and used NAT gateways for outbound internet connectivity.',
     technologies: ['AWS', 'EC2', 'S3', 'RDS', 'VPC', 'Security Groups', 'NAT Gateways'],
-    link: '',
   },
   {
     title: 'Shopfinity E-commerce Project',
